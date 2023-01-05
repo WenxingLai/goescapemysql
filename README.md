223454518205f3e32a7aa4c1c4230fcbec2918f0
wenxinglai <wenxinglai@tencent.com>
2023-01-05 15:41:52 +0800

    added: readme and implementation

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # goescapemysql
+
+Package goescapemysql provides a go function that escape a string as 
+[mysql-server](https://github.com/mysql/mysql-server) 
+[does](https://github.com/mysql/mysql-server/blob/c4f63caa8d9f30b2850672291e0ad0928dd89d0e/mysys/charset.c#L789). 
+
+This function escapes strings by adding backslashes before special characters, such as making the null byte `\0` and 
+the newline byte `\n`. The implementation also converts `%` to `\%` and `_` to `\_`, respectively.
+
+##### Acknowledgement
+
+Package goescapemysql is for personal use only and does not guarantee anything.
+
+##### License
+
+See [WTFPL](http://www.wtfpl.net/txt/copying/).
---
+++ escape.go
@@ -0,0 +1,46 @@
+package goescapemysql
+
+import (
+	"strings"
+)
+
+// RealEscapeString escapes a string as mysql-server does.
+func RealEscapeString(value string) string {
+	var sb strings.Builder
+	// Source: #789 escape_string_for_mysql https://github.com/mysql/mysql-server/blob/5.7/mysys/charset.c
+	// Adding: % and _
+	for _, v := range []byte(value) {
+		switch v {
+		case '\n':
+			sb.WriteByte('\\')
+			sb.WriteByte('n')
+		case '\r':
+			sb.WriteByte('\\')
+			sb.WriteByte('r')
+		case 0:
+			sb.WriteByte('\\')
+			sb.WriteByte('0')
+		case '\\':
+			sb.WriteByte('\\')
+			sb.WriteByte('\\')
+		case '\'':
+			sb.WriteByte('\\')
+			sb.WriteByte('\'')
+		case '"':
+			sb.WriteByte('\\')
+			sb.WriteByte('"')
+		case '\032':
+			sb.WriteByte('\\')
+			sb.WriteByte('Z')
+		case '_':
+			sb.WriteByte('\\')
+			sb.WriteByte('_')
+		case '%':
+			sb.WriteByte('\\')
+			sb.WriteByte('%')
+		default:
+			sb.WriteByte(v)
+		}
+	}
+	return sb.String()
+}
